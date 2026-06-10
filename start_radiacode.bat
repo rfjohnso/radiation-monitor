14f093e22204cbb9f7806371aa277bf90aa9ead1
rfj2n <rfj2n@virginia.edu>
2026-06-10 14:38:20 -0400

    Update start_radiacode.bat
    added note

--- start_radiacode.bat
+++ start_radiacode.bat
@@ -1,4 +1,4 @@
 @echo off
-REM Start the Radiacode-HA bridge silently using pythonw (no console window)
+REM Start the Radiacode-HA bridge silently using pythonw (no console window). Please change repo to private!
 cd /d D:\repo\Radiacode
 C:\Users\lex\miniconda3\pythonw.exe radiacode_ha.py
